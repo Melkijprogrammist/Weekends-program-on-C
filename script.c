a3339bf94dacff75c68f3fa9e86cec2988670777
Uliana <uliana20037@yandex.ru>
2025-01-14 17:49:57 +0300

    Create script.c

---
+++ script.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+int day = 8;
+
+switch(day){
+    case 1:
+    printf("Monday");
+    case 2:
+    printf("Tuesday");
+    case 3:
+    printf("Wenday");
+    case 4:
+    printf("Thursday");
+    case 5:
+    printf("Friday");
+    case 6: 
+    printf("Sarteday");
+    case 7:
+    printf("Sunday");
+    break;
+    default:
+    printf("Looking forward to the Weekend");
+}
+
+}
